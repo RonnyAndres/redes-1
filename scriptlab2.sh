bb70843b334aab8813b46c57a35cd554b88d1082
ronnyandresp <123499619+ronnyandresp@users.noreply.github.com>
2023-01-29 21:21:47 -0600

    Update scriptlab2.sh

--- scriptlab2.sh
+++ scriptlab2.sh
@@ -34,7 +34,13 @@ echo "\$TTL 604800
 ns1        IN  A   192.168.1.1
 correo     IN  A   192.168.1.2
 sistemas   IN  A   192.168.1.3
-respaldo   IN  A   192.168.1.4" >> /etc/bind/db.pantoja.com.co.zone 
+respaldo   IN  A   192.168.1.4
+
+; resolución inversa
+1   IN  PTR ns1.obando.edu.co.
+2   IN  PTR correo.obando.edu.co.
+3   IN  PTR sistemas.obando.edu.co.
+4   IN  PTR respaldo.obando.edu.co." >> /etc/bind/db.pantoja.com.co.zone 
 
 # Crear el archivo de zona para SEGUNDO APELLIDO
 touch /etc/bind/db.obando.edu.co.zone 
@@ -49,47 +55,18 @@ echo "\$TTL 604800
 @   IN  NS  ns1.obando.edu.co.
 @   IN  A   172.16.5.1
 
-1 IN  PTR ns1.pantoja.com.co.
-2 IN  PTR correo.pantoja.com.co.
-3 IN  PTR sistemas.pantoja.com.co.
-4 IN  PTR respaldo.pantoja.com.co." >> /etc/bind/db.obando.edu.co.zone 
+ns1        IN  A   192.168.1.5
+correo     IN  A   192.168.1.6
+sistemas   IN  A   192.168.1.7
+respaldo   IN  A   192.168.1.8
 
-# ----------------------------------- Punto #2 -----------------------------------------------
-# Crear el archivo de zona INVERSO para PRIMER APELLIDO
-touch /etc/bind/db.pantoja.com.co.rev 
-echo "\$TTL 604800
-@   IN  SOA ns1.pantoja.com.co. admin.pantoja.com.co. (
-                  2         ; Serial
-             604800         ; Refresh
-              86400         ; Retry
-            2419200         ; Expire
-             604800 )       ; Negative Cache TTL
-;
-@   IN  NS  ns1.pantoja.com.co.
-@   IN  A   192.168.1.1
+; resolución inversa
+1   IN  PTR ns1.pantoja.com.co.
+2   IN  PTR correo.pantoja.com.co.
+3   IN  PTR sistemas.pantoja.com.co.
+4   IN  PTR respaldo.pantoja.com.co." >> /etc/bind/db.obando.edu.co.zone 
 
-1 IN  PTR ns1.pantoja.com.co.
-2 IN  PTR correo.pantoja.com.co.
-3 IN  PTR sistemas.pantoja.com.co.
-4 IN  PTR respaldo.pantoja.com.co." >> /etc/bind/db.pantoja.com.co.rev
 
-# Crear el archivo de zona INVERSO para SEGUNDO APELLIDO
-touch /etc/bind/db.obando.edu.co.rev
-echo "\$TTL 604800
-@   IN  SOA ns1.pantoja.com.co. admin.pantoja.com.co. (
-                  2         ; Serial
-             604800         ; Refresh
-              86400         ; Retry
-            2419200         ; Expire
-             604800 )       ; Negative Cache TTL
-;
-@   IN  NS  ns1.obando.edu.co.
-@   IN  A   192.168.1.1
-
-5 IN  PTR ns1.obando.edu.co.
-6 IN  PTR correo.obando.edu.co.
-7 IN  PTR sistemas.obando.edu.co.
-8 IN  PTR respaldo.obando.edu.co." >> /etc/bind/db.obando.edu.co.rev
 
 service bind9 restart
 service bind9 status
